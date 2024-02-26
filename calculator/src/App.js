f1b8cccf847df94eae84be825f1ba9f535fff67c
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-02-26 16:57:23 +0530

    calculato

---
+++ calculator/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import ParentContainer from './component/ParentContainer'
+import Display from './component/Display'
+import Buttons from './component/Buttons'
+export default function App() {
+  const [data,setData]=useState('');
+  function handleClick(val){
+    if(val==='='){
+      setData(eval(data));
+    }else if(val==='C'){
+      setData("");
+    }else if(val==='<x'){
+      let res= data+'';
+      setData(res.slice(0,-1));
+    }else{
+      setData(data+val);
+    }
+  }
+  return (
+    <ParentContainer>
+      <Display data={data}/>
+      <Buttons btnClick={handleClick}/>
+    </ParentContainer>
+  )
+}
